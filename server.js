8d3c0ba66db43d6834f3d21cd08011062b2df2d4
Satyam <satyamworks20@gmail.com>
2024-02-22 10:55:35 +0530

    branch created

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
 const port_1 = 1300;
-const port_2 = 1301
+// const port_2 = 1301
 
 // define express apps that run on port_1 and port_2
 let app1, app2
 
 app1= express()
 
-app2= express()
+// app2= express()
 
 app1.get("/",(req,res)=>{
   res.send("APP ONE")
 })
 
-app2.get("/",(req,res)=>{
-  res.send("APP TWO")
-})
+// app2.get("/",(req,res)=>{
+//   res.send("APP TWO")
+// })
 
 if (require.main === module) {
   // add the listen for the express app
@@ -24,9 +24,9 @@ if (require.main === module) {
     console.log("app1")
   })
 
-  app2.listen(port_2,()=>{
-    console.log("app2")
-  })
+  // app2.listen(port_2,()=>{
+  //   console.log("app2")
+  // })
 }
 
 module.exports = {
